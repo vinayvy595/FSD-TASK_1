a6edc5827865447827efd82f34a7bf6167355ed6
vinayvy595 <kavalivinay069@gmail.com>
2025-01-16 18:28:41 +0530

    first commit

---
+++ f1.js
@@ -0,0 +1,34 @@
+var numberVar = 10;
+var stringVar = "Hello";
+var booleanVar = true;
+
+console.log("Using var:");
+console.log("numberVar:", numberVar);
+console.log("stringVar:", stringVar);
+console.log("booleanVar:", booleanVar);
+
+numberVar = 20;
+console.log("Reassigned numberVar:", numberVar);
+
+let numberLet = 5;
+let stringLet = "World";
+let booleanLet = false;
+
+console.log("\nUsing let:");
+console.log("numberLet:", numberLet);
+console.log("stringLet:", stringLet);
+console.log("booleanLet:", booleanLet);
+
+numberLet = 15;
+console.log("Reassigned numberLet:", numberLet);
+
+
+const numberConst = 7;
+const stringConst = "Const";
+const booleanConst = true;
+
+console.log("\nUsing const:");
+console.log("numberConst:", numberConst);
+console.log("stringConst:", stringConst);
+console.log("booleanConst:", booleanConst);
+
